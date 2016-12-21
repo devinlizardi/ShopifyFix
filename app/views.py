ed4b9aef9fabadf3d5b2a9cdd0ea859d184e159b
Devin Lizardi <dlizardi@berkeley.edu>
2016-12-20 16:58:07 -0800

    progress

--- app/views.py
+++ app/views.py
@@ -11,25 +11,27 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+@app.route('/recieve', methods=['GET', 'POST'])
 def fixit(file):
     old = file.read()
     new = ""
     global count_variables
     for i in old:
-        if str(i) in string.printable or i == "'":
+        if str(i) in string.printable:
             new += str(i)
     else:
         count_variables += 1
-    create = open(str(count_variables) + 'fixes.csv','w')
+    create = open(str(count_variables) + ' fixes.csv','w')
     create.write(new)
     create.close()
+    return create
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
+            print('No file part')
             return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
@@ -38,9 +40,7 @@ def upload_file():
             print('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            #filename = secure_filename(file.filename)
-            filename = fixit(file)
+            filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload_file',
-                                    filename=filename))
+            return redirect('/recieve', code=302)(file)
     return render_template('index.html')
